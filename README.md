188dedc40649eaf280feb6ec161dee7db79124c4
Bl1xvan <cflavien1@my.brookdalecc.edu>
2021-08-23 21:54:27 -0400

    still trying to push

---
+++ README.md
@@ -0,0 +1,5 @@
+# rockpaperscissors
+
+The design is based on the arcade version of rock paper scissors that you see in Japan. Click your choice. The Computer choice will appear at the same time. "Win" or "Lose" will be declared at the top
+
+Will add images later
---
+++ script.js
@@ -0,0 +1,45 @@
+const judgement = document.getElementById("judgement");
+const compChoice = document.getElementById("compchoice");
+const compLabel = document.getElementById("comp-label");
+const dimensions = document.getElementById("dimensions");
+
+function checkSize(){
+    const height = this.innerHeight;
+    const width = this.innerWidth;
+    const txt = "H: " + height + " W: " + width;
+    dimensions.innerText = txt;
+}
+
+//go back to the attributes route
+
+//Any way to add points?
+
+const weapons = ["rock", "paper", "scissors"];
+document.addEventListener("click", gameOn);
+
+function gameOn(event){
+if(event.target.className == "yourchoice"){
+        const weapon = randomize();
+        const yourChoice = event.target;
+        compLabel.innerHTML = weapon;
+        displayComp(yourChoice, weapon);
+    }
+}
+
+function randomize(){
+    const randomIndex = Math.floor(Math.random() * weapons.length);
+    return weapons[randomIndex];
+}
+
+function displayComp(yourChoice, weapon){
+    compChoice.innerHTML = `<img src="/images/enemy_`+ weapon +`.png" alt="`+weapon+`">`;
+    const winorlose = yourChoice.getAttribute("data-" + weapon);
+    displayJudge(winorlose);
+}
+
+function displayJudge(winorlose){
+    judgement.innerHTML = winorlose;
+    judgement.setAttribute("class", winorlose);
+    document.getElementById(winorlose).innerText = parseInt(document.getElementById(winorlose).innerText) + 1;
+}
+
